d79f5dee53460c717170292c2fd82c6f58ae0ee8
Mauricio Perdomo <ml.perdomo97@gmail.com>
2021-04-23 21:46:54 -0400

    refactor: Clean MyLSTM class

--- LSTMnn.py
+++ LSTMnn.py
@@ -1,66 +1,93 @@
 import torch
 from torch import nn
+from torch.nn.functional import softmax
+
+BERT_VECTOR_SIZE = 178
+
 
 class MyLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size, input_size_words, hidden_size_words, output_size_words):
+    def __init__(self, word_dimensions, main_hidden_size, output_size, number_of_letters, secondary_hidden_size):
         super(MyLSTM, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
+        self.word_dimensions = word_dimensions
+        self.main_hidden_size = main_hidden_size
         self.output_size = output_size
-        self.input_size_words = input_size_words
-        self.hidden_size_words = hidden_size_words
-        self.output_size_words = output_size_words
+        self.number_of_letters = number_of_letters
+        self.secondary_hidden_size = secondary_hidden_size
 
-        self.lstm_embedding = nn.LSTM(input_size=input_size_words, hidden_size=hidden_size_words, batch_first=True, bidirectional=True)
-        self.linear_from_word = nn.Linear(2*hidden_size_words+178, input_size)
+        # Word representation netword
+        self.word_lstm = nn.LSTM(input_size=self.number_of_letters,
+                                 hidden_size=secondary_hidden_size, batch_first=True)
 
-        self.lstm = nn.LSTM(input_size=self.input_size, hidden_size=self.hidden_size, batch_first=True, bidirectional=True)
-        self.linear_to_tag_classes = nn.Linear(hidden_size*2, self.output_size)
+        self.word_linear = nn.Linear(
+            input_size=secondary_hidden_size, output_size=word_dimensions)
 
-        self.softmax = nn.LogSoftmax(dim=1)
+        # Sentence analysis
+        lstm_input_size = word_dimensions + BERT_VECTOR_SIZE
+        lstm_output_size = output_size*2
+        self.sentence_lstm = nn.LSTM(
+            input_size=lstm_input_size, output_size=lstm_output_size, batch_first=True, bidirectional=True)
 
-    def init_hidden(self, batch_size):
-        hidden_state = torch.randn(2, batch_size, self.hidden_size)
-        cell_state = torch.randn(2, batch_size, self.hidden_size)
+        self.output_layer = nn.Linear(
+            input_size=output_size*2, output_size=output_size)
+
+    def __init_main_hidden(self, batch_size):
+        hidden_state = torch.randn(2, batch_size, self.main_hidden_size)
+        cell_state = torch.randn(2, batch_size, self.main_hidden_size)
         return (hidden_state, cell_state)
 
-    
-    def forward(self, input):
-        batch_size = input.shape[0]
-        
-        input = input.view(-1, 206, 113)
-        no_bert= input[:, :28,: ]
-        bert=input[:, -178:, :]
-        new_bert=torch.empty(bert.shape[0], 178)
-        
-        for i,word in enumerate(bert):
-            zeroes=torch.zeros(1, 113)
-            zeroes[0][0]=1
-            trans=torch.transpose(zeroes, 0, 1)
-            new_bert[i]=torch.matmul(word, trans).squeeze()
-            
-        
-        
-        hidden = self.init_hidden(no_bert.shape[0])
-        words, hidden = self.lstm_embedding(no_bert, hidden)
-        words = words.squeeze()[:, -1, :]
-        
-        
-        
-        #new_words=torch.empty(words.shape[0], self.hidden_size*2+178)
-        bert_words=torch.cat((new_bert, words), dim=1)
-        
-        bert_words = self.linear_from_word(bert_words)
-        bert_words = bert_words.view(batch_size, -1, 50)
-        hidden = self.init_hidden(bert_words.shape[0])
-        bert_words, _ = self.lstm(bert_words, hidden)
-        # words = words.squeeze()[:, -1, :]
-        bert_words = bert_words.reshape(-1, 100)
-        bert_words = self.linear_to_tag_classes(bert_words)
-        
-        tags = self.softmax(bert_words)
-        return tags
+    def __init_secondary_hidden(self, batch_size):
+        hidden_state = torch.randn(1, batch_size, self.secondary_hidden_size)
+        cell_state = torch.randn(1, batch_size, self.secondary_hidden_size)
+        return (hidden_state, cell_state)
 
+    def forward(self, X):
+        batch_size, sentence_length, *_ = X.shape
 
-    # [number_of_sentences, number_of_words, word_feature]
-    # word -> [number_of_letters, features_letter]
+        # Go to word level
+        number_of_words = sentence_length * batch_size
+        X = X.view(number_of_words, -1, self.number_of_letters)
+
+        # Separate word letters from BERT
+        words_lenght = X.shape[1] - BERT_VECTOR_SIZE
+        letters = X[:, :words_lenght, :]
+        bert = self.__get_bert_vectors(X)
+
+        # Words througth lstm
+        hidden = self.__init_main_hidden(batch_size)
+        output, _ = self.word_lstm(letters, hidden)
+
+        # Output from lstm througth linear to get word representations
+        word_representation = output[:, -1, :].squeeze(1)
+        word_representation = self.word_linear(word_representation)
+
+        # Create ultimate vectors :)
+        words_representation = torch.cat((word_representation, bert), dim=1)
+
+        # Go to sentece level
+        sentences = word_representation.view(batch_size, sentence_length, -1)
+
+        # Sentences througth lstm
+        hidden = self.__init_secondary_hidden(batch_size)
+        output, _ = self.sentence_lstm(sentences, hidden)
+
+        # Go to level word
+        words = output.view(batch_size * sentence_length, -1)
+
+        # Words througth last linear layer
+        tags = self.output_layer(words)
+
+        # Get tags probs
+        tags_probs = softmax(tags, 1)
+
+        # Go to sentence level
+        tags_probs = tags_probs.view(batch_size, sentence_length, -1)
+
+        return tags_probs
+
+    def __get_bert_vectors(self, X):
+        padded_bert = X[:, -BERT_VECTOR_SIZE:, :]
+        cleaned_bert = torch.zeros(padded_bert.shape[0], BERT_VECTOR_SIZE)
+        for i, vector in enumerate(padded_bert):
+            for j in range(BERT_VECTOR_SIZE):
+                cleaned_bert[i][j] = vector[j, 0]
+        return cleaned_bert
