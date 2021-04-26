f5bf6d8c5fdea4857789bc14d60ac662d5497c9e
Isabella Sierra <tuquisierra@gmail.com>
2021-04-26 16:17:36 -0400

    fixing encoding

--- postag.py
+++ postag.py
@@ -1,12 +1,13 @@
 import spacy
 
+POS_LIST = ["ADJ", "ADP", "ADV", "AUX", "CONJ", "CCONJ", "DET", "INTJ", "NOUN", "NUM", "PART", "PRON", "PROPN", "PUNCT", "SCONJ", "SYM", "VERB", "X", "SPACE"]
+DEP_LIST = ["ROOT", "acl", "acomp", "advcl", "advmod", "agent", "amod", "appos", "attr", "aux", "auxpass", "case", "cc", "ccomp", "compound", "conj", "cop", "csubj", "csubjpass", "dative", "dep", "det", "dobj", "expl", "intj", "mark", "meta", "neg", "nn", "nmod", "nsubj", "nsubjpass", "oprd", "obj", "obl", "pcomp", "pobj", "poss", "preconj", "prep", "prt", "punct",  "quantmod", "relcl", "root", "xcomp"]
+
 def pos_tag(text):
     nlp=spacy.load('es_core_news_sm')
     doc =nlp(text)
     tags=[]
     for token in doc:
-        tags.append([token.pos, token.dep, text.index(token.head.text), token.head.pos])
-        
-    print(tags)
-    
+        tags.append([POS_LIST.index(token.pos_), DEP_LIST.index(token.dep_), text.index(token.head.text), POS_LIST.index(token.head.pos_)])
+
     return tags
